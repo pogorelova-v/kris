ffcffad941d5ac0baf61599fcbf4e612ce0a9f73
Pogorelova-v <gorelikovavn@gmail.com>
2023-01-25 19:28:17 +0300

    width 1200px

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ let demoActive = document.querySelector('.demo-active');
 let elHeight = demoActive.offsetHeight;
 
 demo.addEventListener('mouseover', ()=> {
-    demoActive.style.transform = 'translateY(' + elHeight + 'px)'
+    demoActive.style.transform = 'translateY(' + elHeight  + 'px)'
 });
 demo.addEventListener('mouseout', ()=> {
     demoActive.style.transform = 'translateY(0px)'
@@ -18,13 +18,8 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 3,
-    spaceBetween: 10,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
+    spaceBetween: 10,  
+    
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
